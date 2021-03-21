37be189e4f96fcb7a5337c3c76fe61abb341f70b
vubao2303 <bbbvut@gmail.com>
2021-03-20 22:59:31 -0700

    for loop practice

--- course-content/03-javascript/code-drills/00-required-code-drills/04-core-js-arrays-2/challenge-prompt.js
+++ course-content/03-javascript/code-drills/00-required-code-drills/04-core-js-arrays-2/challenge-prompt.js
@@ -1,26 +1,27 @@
 // declare a variable named ourArray and assign an empty array to it
 
-
+let ourArr = [];
 // we can `push` values into an array with the push method. Documentation can be found here:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 
 // Let's push the number 1 into ourArray
+ourArr.push(1)
 
 
 // push the string "hello" into ourArray
-
+ourArr.push("hello")
 
 // push the boolean false into ourArray
 
-
+ourArr.push(false)
 // push the number 84 into ourArray
-
+ourArr.push(84)
 
 // push the string "world" into our Array
-
+ourArr.push("world")
 
 // console log ourArray
-
+console.log(ourArr)
 
 
 // This seperator is here to make your console logs a little easier to read 
@@ -29,12 +30,12 @@ console.log("--------------------------Seperator 1--------------------------");
 
 // declare a variable named oneToTen and assign it an array containing the numbers from 1 to 10
 
-
+oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console log the element at index 4
 
-
+console.log(oneToTen[4])
 // console log the element at index 7
-
+console.log(oneToTen[7])
 
 
 
@@ -45,24 +46,50 @@ console.log("--------------------------Seperator 2--------------------------");
 // We've declared this array for you
 var numArray = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37];
 // iterate through numArray and create an individual console log for every number greater than 50
-
+// while (i > 50) {
+//   console.log(numArray[i])
+// }
+for (var i = 0; i < numArray.length; i++) {
+  if (numArray[i] > 50) {
+    console.log(numArray[i])
+  }
+};
 
 
 // iterate through numArray and create an alert that tells us 
 // the index of all the occurances of the number 11
 
-
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] === 11) {
+    alert(i)
+  }
+}
 
 // iterate through numArray and console log the sum of all the numbers
-
+var sum = 0;
+for (let i = 0; i < numArray.length; i++) {
+  sum += numArray[i]
+}
+console.log("the sum is " + sum)
 
 
 // iterate through numArray and console log the sum of all the numbers greater than 50
-
-
+var bigSum = 0;
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] > 50) {
+    bigSum += numArray[i];
+  }
+}
+console.log("sum of big number is " + bigSum)
 
 // iterate through numArray and console log the sum of all the even numbers
-
+var evenSum = 0;
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] % 2 === 0) {
+    evenSum += numArray[i]
+  }
+}
+console.log(evenSum)
 
 
 // This seperator is here to make your console logs a little easier to read 
@@ -71,22 +98,43 @@ console.log("--------------------------Seperator 3--------------------------");
 
 // We've declared this array for you
 var fruits = [
-  "Apple", "Orange", "Banana", "Pomelo", "Apple", "Kiwi", "Peach", "Banana", "Grape", "Tomato", 
+  "Apple", "Orange", "Banana", "Pomelo", "Apple", "Kiwi", "Peach", "Banana", "Grape", "Tomato",
   "Kiwi", "Apple", "Watermelon", "Lemon", "Pomelo", "Apple", "Banana", "Peach", "Apricot", "Grape"];
 // iterate through fruits and console log the number of times "Apple" appears in the array
-
+var count = 0;
+for (let i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "Apple") {
+    count++
+  }
+}
+console.log(count)
 
 
 // iterate through fruits and console log the number of times "Peach" appears in the array
-
+let peachCount = 0;
+for (let i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "Peach") {
+    peachCount += 1
+  }
+}
+console.log(peachCount)
 
 
 // iterate through fruits and console log the number of fruits that start with "P" in the array
-
+let pCount = 0;
+for (let i = 0; i < fruits.length; i++) {
+  if (fruits[i][0] === "P") {
+    pCount++
+  }
+}
+console.log(pCount)
 
 
 // create a new empty array named uniqueFruits
+var uniqueFruits = [];
+for (let i = 0; i < fruits.length; i++) {
 
+}
 
 // iterate through fruits and populate uniqueFruits with only unique values from fruits
 // you should a shorter array without repeated values
@@ -149,7 +197,7 @@ console.log("     odd numbers      ");
 
 // another seperator for your convenience
 console.log("     sum of multiples of 3      ");
-// iterate through twoDimArray and the arrays inside of it and 
+// iterate through twoDimArray and the arrays inside of it and
 // console log the sum of all the numbers that are a multiple of 3
 
 
