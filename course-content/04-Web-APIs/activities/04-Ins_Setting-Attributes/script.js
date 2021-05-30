6421589936c3ee079d1a2591fe5ab7fff22612d5
vubao2303 <bbbvut@gmail.com>
2021-05-30 14:07:34 -0700

    setAtribute src for images

--- course-content/04-Web-APIs/activities/04-Ins_Setting-Attributes/script.js
+++ course-content/04-Web-APIs/activities/04-Ins_Setting-Attributes/script.js
@@ -22,7 +22,7 @@ console.log(pTags);
 pTags[0].setAttribute("style", "font-size: 65px;");
 changeP.setAttribute("style", "color:blue; border:2px solid black;");
 aTags[0].setAttribute("href", "https://github.com");
-imgEl[0].setAttribute("src", "images/image_1.jpg");
+imgEl[0].setAttribute("src", "image_1.jpg");
 imgEl[0].setAttribute("style", "width:500px; height:200px;");
 
 for (var i = 0; i < divTags.length; i++) {
---
+++ course-content/04-Web-APIs/activities/05-Stu_Setting-Attributes/Unsolved/script.js
@@ -0,0 +1,15 @@
+var imageElement = document.querySelectorAll("img")
+
+// var imgEl = document.querySelectorAll("img");
+// * Give a`src` and`alt` attribute to the 3 provided`img` tags.
+imageElement[0].setAttribute("src", "img1.jpeg")
+
+imageElement[1].setAttribute("src", "img2.jpg")
+
+imageElement[2].setAttribute("src", "img3.jpg")
+
+// imgEl[0].setAttribute("src", "image_1.jpg");
+
+  // * Give a`href` attribute to the 3 provided`a` tags.
+
+  // * Add styles via`setAttribute` to make the page look decent.Recommended: margins and padding.
